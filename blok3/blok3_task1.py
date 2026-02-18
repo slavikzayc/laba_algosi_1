c5c925d3318245b2de747f9829f1df3db813d42c
p0sd7 <0-locals-daring@icloud.com>
2026-02-18 16:55:21 +0300

    добавил legacy merge на основе материала лекции, пока без комментариев

--- blok3/blok3_task1.py
+++ blok3/blok3_task1.py
@@ -2,6 +2,41 @@ import time
 
 start_time = time.perf_counter()
 
+def legacy_merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+
+    L = [0] * (n1 + 1)
+    R = [0] * (n2 + 1)
+
+    for i in range(n1):
+        L[i] = A[p + i]
+
+    for j in range(n2):
+        R[j] = A[q + 1 + j]
+
+    L[n1] = float('inf')
+    R[n2] = float('inf')
+
+    i = 0
+    j = 0
+
+    for k in range(p, r + 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+def legacy_merge_sort(A, p, r):
+    if p < r:
+        q = (p + r) // 2  # целочисленное деление (floor)
+        legacy_merge_sort(A, p, q)
+        legacy_merge_sort(A, q + 1, r)
+        legacy_merge(A, p, q, r)
+    return A
+
 def merge(left_list, right_list):
     sorted_list = []
     left_list_index = right_list_index = 0
@@ -49,7 +84,7 @@ raw_list_of_numbers = input_data[1].split(" ") #и саму последоват
 for number in raw_list_of_numbers: #Проходим циклом по массиву строк
     list_of_numbers.append(int(number)) #Добавляем элементы в массив, где элементы уже будут типа int
 
-list_of_numbers = merge_sort(list_of_numbers) #Производим сортировку
+list_of_numbers = legacy_merge_sort(list_of_numbers, 0, list_length-1) #Производим сортировку
 
 for number in list_of_numbers: #Проходим циклом по отсортированному массиву
     output_data += str(number)+' ' #Добавляем отсортированную последовательность в строку
