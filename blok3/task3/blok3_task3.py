e1cf64312d959b79e97e1c379c59d6936890e591
p0sd7 <0-locals-daring@icloud.com>
2026-02-24 16:08:18 +0300

    ну бля потом доделаю крч

--- blok3/task3/blok3_task3.py
+++ blok3/task3/blok3_task3.py
@@ -7,6 +7,19 @@ time_start = time.perf_counter()
 #модифицировать сортировку слиянием, посчитать количество инверсий в массиве из 1<n<10^5 чисел не превосходящих 10^5
 
 def get_input(filename):
+    list_of_numbers = []
+    with open(filename) as f:  # читаем файл
+        input_data = f.readlines()
+
+    list_length = int(input_data[0].strip())  #
+    raw_list_of_numbers = input_data[1].strip().split()  # обрабатываем строки
+
+    for number in raw_list_of_numbers:
+        if number:  # проверяем, что строка не пустая
+            list_of_numbers.append(int(number))
+    return [list_length, list_of_numbers]
+
+
 
 
 time_end = time.perf_counter()
